55a863e50a30570a150403e262c620a0b5b1263d
Jinzhu <wosmvp@gmail.com>
2011-11-16 18:59:49 +0800

    support change auth mode and auth text

--- lib/rack-ntlm/ntlm.rb
+++ lib/rack-ntlm/ntlm.rb
@@ -9,7 +9,9 @@ module Rack
       @config = {
         :uri_pattern => /\//,
         :port => 389,
-        :search_filter => "(sAMAccountName=%1)"
+        :search_filter => "(sAMAccountName=%1)",
+        :auth_mode => "NTLM"
+        :auth_text => "change it by change config[:auth_text]"
       }.merge(config)
     end
 
@@ -26,7 +28,7 @@ module Rack
 
     def call(env)
       if env['PATH_INFO'] =~ @config[:uri_pattern] && env['HTTP_AUTHORIZATION'].blank?
-        return [401, {'WWW-Authenticate' => "NTLM"}, []]
+        return [401, {'WWW-Authenticate' => @config.auth_mode}, [@config.auth_text]]
       end
 
       if /^(NTLM|Negotiate) (.+)/ =~ env["HTTP_AUTHORIZATION"]
@@ -35,7 +37,7 @@ module Rack
 
         if message.type == 1 
           type2 = Net::NTLM::Message::Type2.new
-          return [401, {"WWW-Authenticate" => "NTLM " + type2.encode64}, []]
+          return [401, {"WWW-Authenticate" => "#{@config.auth_mode} " + type2.encode64}, [@config.auth_text]]
         end
 
         if message.type == 3 && env['PATH_INFO'] =~ @config[:uri_pattern]
